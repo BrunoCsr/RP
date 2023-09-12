d708553dc5a40306c4fda773dafea7752099c17d
BrunoCsr <bruno.c@geb.inatel.br>
2023-09-12 15:13:36 +0200

    python file created

--- temp.py
+++ temp.py
@@ -2,7 +2,26 @@
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-f0 = open(TEMP_PATH + "temp", "r")
-temp = f0.read()
-print(temp)
+#f0 = open(TEMP_PATH + "temp", "r")
+#temp = f0.read()
+#print(temp)
+
+def readTemp():
+    f0 = open(TEMP_PATH + "temp", "r")
+    temp = float(f0.read())
+    print(temp*0.001)
+#readTemp()
+
+
+def avgTemp(n):
+    
+    temp = 0
+    
+    for i in range(0,n):
+        f0 = open(TEMP_PATH + "temp", "r")
+        temp += float(f0.read())
+    
+    print(temp/n*0.001)
+
+
 
